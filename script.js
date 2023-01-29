70fed7dbe5147edb45d283c1f2584632f7e37b67
DarthCyrus <Cyrus@t-online.de>
2023-01-29 19:30:15 +0100

    mouse animation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hiddenElements = document.getElementsByClassName("hidden");
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-        entry.target.classList.add("fade-in");
+      entry.target.classList.add("fade-in");
     }
   });
 });
@@ -21,3 +21,56 @@ function copyToClipboard(str) {
   // Alert the copied text
   alert(str + " Wurde in deine Zwischenablage gespeichert");
 }
+
+// Mouse animation
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".mouse-animation-circle");
+
+const colors = [
+  "#56070c",
+  "#56070c",
+  "#56070c",
+  "#7b2521",
+  "#883028",
+  "#943a2f",
+  "#a04536",
+  "#ac503c",
+  "#b95b43",
+  "#c5674a",
+  "#d17351",
+  "#dd7e57",
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
